4cdb98c359875de48073cb8564bbb2c6681527b6
daughlas <daughlas@gmail.com>
2020-06-24 11:22:14 +0800

    test

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -12,126 +12,126 @@ import CitySelector from '../common/CitySelector';
 import DateSelector from '../common/DateSelector';
 import { h0 } from '../common/fp';
 import {
-    exchangeFromTo,
-    showCitySelector,
-    hideCitySelector,
-    fetchCityData,
-    setSelectedCity,
-    showDateSelector,
-    hideDateSelector,
-    setDepartDate,
-    toggleHighSpeed,
+  exchangeFromTo,
+  showCitySelector,
+  hideCitySelector,
+  fetchCityData,
+  setSelectedCity,
+  showDateSelector,
+  hideDateSelector,
+  setDepartDate,
+  toggleHighSpeed,
 } from './actions';
 
 function App(props) {
-    const {
-        from,
-        to,
-        isCitySelectorVisible,
-        cityData,
-        isLoadingCityData,
-        dispatch,
-        departDate,
-        isDateSelectorVisible,
-        highSpeed,
-    } = props;
+  const {
+    from,
+    to,
+    isCitySelectorVisible,
+    cityData,
+    isLoadingCityData,
+    dispatch,
+    departDate,
+    isDateSelectorVisible,
+    highSpeed,
+  } = props;
 
-    // TODO why
-    // useCallback 避免没有必要的重渲染
-    const onBack = useCallback(() => {
-        window.history.back();
-    }, []);
+  // TODO why
+  // useCallback 避免没有必要的重渲染
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
 
-    const cbs = useMemo(() => {
-        return bindActionCreators(
-            {
-                exchangeFromTo,
-                showCitySelector,
-            },
-            dispatch
-        );
-    }, []);
-
-    const citySelectorCbs = useMemo(() => {
-        return bindActionCreators(
-            {
-                onBack: hideCitySelector,
-                fetchCityData,
-                onSelect: setSelectedCity,
-            },
-            dispatch
-        );
-    }, []);
-
-    const departDateCbs = useMemo(() => {
-        return bindActionCreators(
-            {
-                onClick: showDateSelector,
-            },
-            dispatch
-        );
-    }, []);
-
-    const dateSelectorCbs = useMemo(() => {
-        return bindActionCreators(
-            {
-                onBack: hideDateSelector,
-            },
-            dispatch
-        );
-    }, []);
-
-    const onSelectDate = useCallback(day => {
-        if (!day) {
-            return;
-        }
-        if (day < h0()) {
-            return;
-        }
-        dispatch(setDepartDate(day));
-        dispatch(hideDateSelector());
-    }, []);
-
-    const highSpeedCbs = useMemo(() => {
-        return bindActionCreators(
-            {
-                toggle: toggleHighSpeed,
-            },
-            dispatch
-        );
-    }, []);
-
-    return (
-      <div>
-        <div className="header-wrapper">
-          <Header title="火车票" onBack={onBack} />
-        </div>
-        <form className="form" action="./query.html">
-          <Journey from={from} to={to} {...cbs} />
-          <DepartDate time={departDate} {...departDateCbs} />
-          <HighSpeed highSpeed={highSpeed} {...highSpeedCbs} />
-          <Submit />
-        </form>
-        <CitySelector
-                show={isCitySelectorVisible}
-                cityData={cityData}
-                isLoading={isLoadingCityData}
-                {...citySelectorCbs}
-            />
-        <DateSelector
-                show={isDateSelectorVisible}
-                {...dateSelectorCbs}
-                onSelect={onSelectDate}
-            />
-      </div>
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector,
+      },
+      dispatch
     );
+  }, []);
+
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideCitySelector,
+        fetchCityData,
+        onSelect: setSelectedCity,
+      },
+      dispatch
+    );
+  }, []);
+
+  const departDateCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onClick: showDateSelector,
+      },
+      dispatch
+    );
+  }, []);
+
+  const dateSelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideDateSelector,
+      },
+      dispatch
+    );
+  }, []);
+
+  const onSelectDate = useCallback(day => {
+    if (!day) {
+      return;
+    }
+    if (day < h0()) {
+      return;
+    }
+    dispatch(setDepartDate(day));
+    dispatch(hideDateSelector());
+  }, []);
+
+  const highSpeedCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggle: toggleHighSpeed,
+      },
+      dispatch
+    );
+  }, []);
+
+  return (
+    <div>
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack} />
+      </div>
+      <form className="form" action="./query.html">
+        <Journey from={from} to={to} {...cbs} />
+        <DepartDate time={departDate} {...departDateCbs} />
+        <HighSpeed highSpeed={highSpeed} {...highSpeedCbs} />
+        <Submit />
+      </form>
+      <CitySelector
+        show={isCitySelectorVisible}
+        cityData={cityData}
+        isLoading={isLoadingCityData}
+        {...citySelectorCbs}
+      />
+      <DateSelector
+        show={isDateSelectorVisible}
+        {...dateSelectorCbs}
+        onSelect={onSelectDate}
+      />
+    </div>
+  );
 }
 
 export default connect(
-    function mapStateToProps(state) {
-        return state;
-    },
-    function mapDispatchToProps(dispatch) {
-        return { dispatch };
-    }
+  function mapStateToProps(state) {
+    return state;
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch };
+  }
 )(App);
--- src/index/DepartDate.jsx
+++ src/index/DepartDate.jsx
@@ -6,34 +6,34 @@ import './DepartDate.css';
 import { h0 } from '../common/fp';
 
 export default function DepartDate(props) {
-    const { time, onClick } = props;
+  const { time, onClick } = props;
 
-    // 性能优化， 为了 useMemo
-    const h0OfDepart = h0(time);
+  // 性能优化， 为了 useMemo
+  const h0OfDepart = h0(time);
 
-    const departDateString = useMemo(() => {
-        console.log(dayjs(time).format('YYYY-MM-DD'));
-        return dayjs(time).format('YYYY-MM-DD');
-    }, [h0OfDepart]);
+  const departDateString = useMemo(() => {
+    console.log(dayjs(time).format('YYYY-MM-DD'));
+    return dayjs(time).format('YYYY-MM-DD');
+  }, [h0OfDepart]);
 
-    const departDate = new Date(h0OfDepart);
-    const isTody = h0OfDepart === h0();
+  const departDate = new Date(h0OfDepart);
+  const isTody = h0OfDepart === h0();
 
-    const weekString =
-        '周' +
-        ['日', '一', '二', '三', '四', '五', '六'][departDate.getDay()] +
-        (isTody ? '（今天）' : '');
+  const weekString =
+    '周' +
+    ['日', '一', '二', '三', '四', '五', '六'][departDate.getDay()] +
+    (isTody ? '（今天）' : '');
 
-    return (
-      <div className="depart-date" onClick={onClick}>
-        <input type="hidden" name="date" value={departDateString} />
-        {departDateString}
-        <span className="depart-week">{weekString}</span>
-      </div>
-    );
+  return (
+    <div className="depart-date" onClick={onClick}>
+      <input type="hidden" name="date" value={departDateString} />
+      {departDateString}
+      <span className="depart-week">{weekString}</span>
+    </div>
+  );
 }
 
 DepartDate.propTypes = {
-    time: PropTypes.number.isRequired,
-    onClick: PropTypes.func.isRequired,
+  time: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
